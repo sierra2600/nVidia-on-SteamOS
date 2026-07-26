cbb301615766c07e65f735a9ccd93e1e9e9d9187
sierra2600 <147783105+sierra2600@users.noreply.github.com>
2026-07-26 00:05:24 -0500

    Update InstallHelper.sh

--- InstallHelper.sh
+++ InstallHelper.sh
@@ -11,11 +11,8 @@ sudo steamos-session-select plasma-x11-persistent
 # Initialize the Arch Package Manager Key File
 sudo pacman-key --init
 
-# Fetch and populate the Arch Package Manager Key File with the Arch Linux key
-sudo pacman-key --populate archlinux
-
-# Fetch and populate the Arch Package Manager Key File with the Valve SteamOS Holo key
-sudo pacman-key --populate holo
+# Fetch and populate the Arch Package Manager Key File with the Arch Linux and Valve SteamOS Holo key
+sudo pacman-key --populate archlinux holo
 
 # Use the Arch Linux Package Manager to download the minimal developer tools and the referenced Linux headers for your specific build of the custom SteamOS kernel
 sudo pacman -Syyu --needed base-devel linux-neptune-616-headers
@@ -23,7 +20,7 @@ sudo pacman -Syyu --needed base-devel linux-neptune-616-headers
 # It's been found that this one package NEEDS TO BE RE/INSTALLED AFTER "base-devel linux-neptune-616-headers" otherwise the nVidia driver won't see it along with "linux-neptune-616-headers" for some odd reason
 sudo pacman -Syyu --needed glibc
 
-# Download the Linux x86_64 (64 bit) nVidia driver for your specific GPU from nVidias website (YES. I KNOW. It goes against what everyone says about installing the one that comes with the distro but this does work and it is up to date)
+# Download the Linux x86_64 (64 bit) nVidia driver (in this case: https://us.download.nvidia.com/XFree86/Linux-x86_64/595.71.05/NVIDIA-Linux-x86_64-595.71.05.run ) for your specific GPU from nVidias website (YES. I KNOW. It goes against what everyone says about installing the one that comes with the distro but this does work and it is up to date)
 # This version of the driver - that needs to be downloaded from nVidias website - has been tested to work without complaint on both a 2080 SUPER and a 4060 Ti
 sudo chmod +x ./NVIDIA-Linux-x86_64-595.71.05.run
 
@@ -37,3 +34,4 @@ sudo mkinitcpio -P
 sudo systemctl enable nvidia-suspend.service nvidia-hibernate.service nvidia-resume.service
 
 # Reboot your computer by going to the Application Launcher and Restart and Restart, once you returned to your desktop, check that it says what your nVidia GPU is in the About this System under Graphics Processor and it may have a discrete tag next to it
+sudo pacman -R linux-neptune-616-headers pahole m4 autoconf automake bison debugedit fakeroot flex libmpc libisl gcc groff make patch pkgconf texinfo base-devel
